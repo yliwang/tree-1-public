d888ee7bb4052a2979b76ece0f82e4936862e5fd
yliwang <1220264221@qq.com>
2021-06-21 12:18:43 +0800

    Update tree.h

--- tree.h
+++ tree.h
@@ -13,3 +13,20 @@ struct binary_tree
 	struct node *root;
 };
 
+/* interface declaration */
+struct node *build_tree( T v, struct node *left_child, struct node *right_child );
+struct node *empty_tree();
+
+int size( struct node *t );
+int height( struct node *t );
+
+bool is_empty( struct node *t );
+bool is_containing( struct node *t, T v );
+
+T root_value( struct node *t );
+
+struct node *left_subtree( struct node *t );
+struct node *right_subtree( struct node *t );
+
+
+
